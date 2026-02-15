7597eb1c8f5846bbc6c0d3626630fbb83fb92634
CirlaAndrei <cirlaandrei7@yahoo.com>
2026-02-15 12:04:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
+// Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Contact form routes
+Route::post('/contact', [ContactController::class, 'store']);
+Route::get('/admin/submissions', [ContactController::class, 'index'])->name('contact.index');
+Route::delete('/admin/submissions/{id}', [ContactController::class, 'destroy']);
+Route::get('/admin/submissions/export', [ContactController::class, 'export']);
